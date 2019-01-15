976c5687eb8b7ba0f85554629a7bbbf9028b87a3
Per Kristian <kristiansvanberg@gmail.com>
2019-01-15 15:03:25 +0100

    PK
    lagt till Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
