92dc173e4c673e80391fd6a3b64cc87b2f002582
Per Kristian <kristiansvanberg@gmail.com>
2019-01-16 13:53:54 +0100

    PK
    Lagt till modal för att visa mer info om film

--- src/components/FilmInfo.vue
+++ src/components/FilmInfo.vue
@@ -12,8 +12,26 @@
     <p>{{text}}</p>
     <div class="button-group">
       <button>Order now</button>
-      <button>Read more</button>
+         <button data-toggle="modal" data-target="#ModalCenter">Read more</button> <!--ta upp modal!!! -->
     </div>
+    
+    <!-- Här börjar modal -->
+    <div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content inner-modal">
+            <div class="modal-header">
+               <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+                  ...
+            </div>
+            <div class="modal-footer"></div>
+         </div>
+      </div>
+   </div>
   </div>
 </template>
 
@@ -42,6 +60,7 @@
         
   
 }
+
 
 .button-group {
     margin: auto;
