0035eb1b46a019636bd0094a6cbc5eea534f5a67
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-01-17 11:14:12 +0100

    confirm order sida start

--- src/views/ConfirmOrder.vue
+++ src/views/ConfirmOrder.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="order">
+    <Navbar></Navbar>
     <h1>orderbekräftelsen goes here!</h1>
+    <Footer></Footer>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "order",
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
