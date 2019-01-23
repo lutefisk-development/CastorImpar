ff91c508ad5da2167d4fef7aa5b30583187820b4
Per Kristian <kristiansvanberg@gmail.com>
2019-01-23 14:59:09 +0100

    PK
    Skapat component för Registrera mig och Logga in.
    lagt till testsida och rout till den (ska tas bort när klar med testing)

--- src/router.js
+++ src/router.js
@@ -64,6 +64,14 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "calender" */ "./views/Calender.vue")
+    },
+    {
+      path: "/Test",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "Test" */ "./views/Test.vue")
     }
 
   ]
---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+      <Login></Login>
+      <RegMember></RegMember>
+      <Footer></Footer>
+   </div>
+</template>
+
+<script>
+import Login from '@/components/LoginComponent.vue'
+import RegMember from '@/components/RegMemberComponent.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+   name: 'Test',
+   components: {
+      Login,
+      RegMember,
+      Footer
+   }
+}
+</script>
+
