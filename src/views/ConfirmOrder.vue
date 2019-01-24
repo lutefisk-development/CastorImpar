7d987cdc86e31fddbc27dfb64f09ba8f24db5c3b
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-01-24 09:27:46 +0100

    tar bor extra navbar

--- src/views/ConfirmOrder.vue
+++ src/views/ConfirmOrder.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="order">
-    <Navbar></Navbar>
     <confirmOrder></confirmOrder>
     <Footer></Footer>
   </div>
@@ -8,14 +7,12 @@
 
 <script>
 // @ is an alias to /src
-import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue";
 import ConfirmOrder from "@/components/ConfirmOrderComp.vue";
 
 export default {
   name: "order",
   components: {
-    Navbar,
     Footer,
     ConfirmOrder
   }
