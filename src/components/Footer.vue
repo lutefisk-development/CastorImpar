7ddb7e4b73f4fb9cf9426fc5076aae9f46e5f18f
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-01-27 18:43:14 +0100

    JM
    ny footer utan länkar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,16 @@
 <template>
   <nav class="footer navbar navbar-expand-lg navbar-bark">
     <div class="footer-box">
-      <div class="links col-sm-auto">
-        <a class="row" href="#">{{ link1 }}</a>
-        <a class="row" href="#">{{ link2 }}</a>
-        <a class="row" href="#">{{ link3 }}</a>
+      <div>
+        <p>© 2019 Castor Impar. All Rights Reserved.</p>
       </div>
-      <div class="links col-sm-auto">
-        <a class="row" href="https://sv-se.facebook.com/" target="_blank">{{ link4 }}</a>
-        <a class="row" href="https://www.instagram.com/?hl=sv" target="_blank">{{ link5 }}</a>
-      </div>
-  </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: "Footer",
-  data() {
-    return {
-      link1: "Kontakta kundservice",
-      link2: "Medlemsvilkor",
-      link3: "Medlemsförmåner",
-      link4: "Följ oss på Facebook",
-      link5: "Följ oss på Instragram"
-    };
-  }
+  name: "Footer"
 };
 </script>
 
@@ -37,6 +22,7 @@ export default {
 .footer-box {
   margin: auto;
   border-top: 4px solid #c2992a;
+  height: 15vh;
 }
 a {
   color: white;
