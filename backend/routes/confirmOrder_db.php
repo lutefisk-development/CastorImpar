548c39f174fd4d1b83f59e951d2f6aeda496fdd0
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-01-28 12:00:45 +0100

    JM

--- backend/routes/confirmOrder_db.php
+++ backend/routes/confirmOrder_db.php
@@ -2,8 +2,8 @@
 include('../boot.php');
 
 // vi hämtar svaret ifrån databasen
-$statement = $db->query("SELECT 
-bookings.id, movies.title, schedule.date_time, cinema.name, cinema.city, prices.name, prices.price 
+$statement = $db->query(
+"SELECT bookings.id, movies.title, schedule.date_time, cinema.name, cinema.city, prices.name, prices.price 
 FROM movies, schedule, cinema, prices, bookings 
 WHERE movies.id = schedule.film_id");
 $statement->execute();
--- src/components/ConfirmOrderComp.vue
+++ src/components/ConfirmOrderComp.vue
@@ -44,7 +44,7 @@ export default {
   data() {
     return {
       order: false,
-      bookingNr: 0
+      bookingNr: 55
     };
   },
   created() {
