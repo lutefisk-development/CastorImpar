e40e3b02fc67efcdbf5d9589bbe8e5f950e9335b
Malin <malinfalk87@gmail.com>
2019-01-28 13:12:04 +0100

    uppdateringar frontend

--- src/components/MyPagesComponent.vue
+++ src/components/MyPagesComponent.vue
@@ -3,7 +3,7 @@
  <div class="MyPages">
     <div container-mypage>
               <div class="space mt-4"><br></div>
-               <div class="profile col-md-6 col-sm-8">
+               <div class="profile col col-md-4 col-lg-4">
                 <section class="card profile mb-4 text-center">
                  <div class="card-mypage p-4">
                    <div class="card-header my-page-head">
@@ -20,34 +20,34 @@
               </div><!--end profile-->
 
 
-        <div class="bokningar col-md-6 col-sm-8">
+        <div class="bokningar col">
           <section class="card booking mb-4 p-3">
           <div class="rubrik"><h5>Bokningshistorik</h5></div>
-       <table class="table bokning-tabell">
+       <table class="table bokning-tabell m-0">
         <tbody>
 
               <tr>
               <td><b>24/1</b></td>
               <td><b>Aquaman</b></td>
-              <td><b>22.30 Filmstaden</b></td>
+              <td><b>22.30 Salong 2</b></td>
               <td><b>85kr</b></td>
             </tr>
             <tr>
               <td>12/12</td>
               <td>Grindewalds Brott</td>
-              <td>18.15 Filmstaden</td>
+              <td>18.15 Salong 2</td>
               <td>85kr</td>
           </tr>
           <tr>
               <td>11/10</td>
               <td>Lord of the rings</td>
-              <td>19.30 Filmsatden</td>
+              <td>19.30 Salong 1</td>
               <td>85kr</td>
           </tr>
           <tr>
               <td>6/4</td>
               <td>King Kong</td>
-              <td>21.15 Filmstaden</td>
+              <td>21.15 Salong 2</td>
               <td>85kr</td>
           </tr>
         </tbody>
@@ -55,17 +55,50 @@
           </section>
 </div>
 
-    <div class="poang-mypage col-md-6 col-sm-8 mt-4">
+    <div class="poang-mypage col mt-4">
           <section class="poang-kort card mb-4 text-center">
             <div class="card-poang p-4">
               <div class="card-header poang-head row m-0">
                 <div class="col-6 poang-text"><h5>Poäng:</h5></div>
-                <div class="col-6 poang-siffror"><h5>00000</h5></div>
+                <div class="col-6 poang-siffror"><h5>250</h5></div>
               </div>
               <img class="card-img bonus img-fluid" :src="popcorn" alt="bonus">
               <div class="card-footer poang-foot">
                 <p class="card-text bonus-text pt-2"><p><b>Du har just nu tjänat ihop till:</b><br>25% rabatt på Popcorn Large</p>
               </div>
+                            <table class="table poang-tabell m-0">
+        <tbody>
+
+              <tr>
+              <td>200 poäng</td>
+              <td>25% stor popcorn</td>
+            </tr>
+            <tr>
+              <td>400 poäng</td>
+              <td>15% biobiljett</td>
+          </tr>
+          <tr>
+              <td>600 poäng</td>
+              <td>30% stora snacksmenyn</td>
+          </tr>
+          <tr>
+              <td>800 poäng</td>
+              <td>40% biobiljett</td>
+          </tr>
+          <tr>
+              <td>1000 poäng</td>
+              <td>50% biobiljett</td>
+  </tr>
+          <tr>
+              <td>1200 poäng</td>
+              <td>2 biobiljetter och stor popcorn</td>
+  </tr>
+        </tbody>
+      </table>
+      <hr>
+
+      <p class="card-text poang-info pt-2">* För varje 100 kronors köp sparar du som medlem 50 poäng. Detta innebär att du vid exempelvis ett köp på 239 kr tjänar in 100 poäng. Detta system ger snabba rabatter vid besök hos Filmvisarna!</p>
+      <h6>Välkommen på bio!</h6>
             </div><!--end card-mypage-->
           </section>
         </div>
@@ -115,6 +148,15 @@ text-align: left;
 	text-align: right;
 }
 
+.poang-tabell{
+	font-size: 0.7em;
+}
+
+.poang-info{
+	font-size: 0.8em;
+	text-align: left;
+	font-style: italic;
+}
 
 </style>
 
