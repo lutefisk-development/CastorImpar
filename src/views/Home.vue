370d3d0a2b79bafc7c5eda72156d92d33a7afc22
Maria Wallin <maria@dwall.in>
2019-01-29 10:08:59 +0100

    removed component not in use

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,6 @@
   
     <MovieSelector></MovieSelector>
     <Movieholder></Movieholder>
-    <upcomingMovies></upcomingMovies>
    
     <!--<img alt="Vue logo" src="../assets/logo.png">-->
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
@@ -18,14 +17,12 @@ import HelloWorld from "@/components/HelloWorld.vue";
 import Footer from "@/components/Footer.vue";
 import MovieSelector from "@/components/movieChoiseBox.vue";
 import Movieholder from "@/components/Movieholder.vue";
-import upcomingMovies from "@/components/upcomingMovies.vue";
 
 export default {
   name: "home",
   components: {
      MovieSelector,
      Movieholder,
-     upcomingMovies,
      HelloWorld,
     Footer,
     
