0cafc0f7679a017dd700acb9b59d6bcfa1f8b171
Joakim Malmberg <jmalmberg.web@gmail.com>
2019-01-30 17:59:49 +0100

    JM
    ändrad till nya databasen

--- backend/routes/confirmOrder_db.php
+++ backend/routes/confirmOrder_db.php
@@ -4,7 +4,7 @@ include('../boot.php');
 // vi hämtar svaret ifrån databasen
 $statement = $db->query(
 "SELECT bookings.id, movies.title, cinema.name, cinema.city, prices.name, prices.price,
- booked_seats.seatnumber, booked_seats.rownumber, schedule.date_time, schedule.salon_id, salons.salon_name
+ booked_seats.seatnumber, booked_seats.row_nr, schedule.date_time, schedule.salon_id, salons.salon_name
 FROM movies, schedule, cinema, prices, bookings, booked_seats, salons 
 WHERE movies.id = schedule.film_id 
 AND prices.id = booked_seats.price_id 
