990b397ece1cb07da6bc5a72c798da5b842304bb
Per Kristian <kristiansvanberg@gmail.com>
2019-01-31 10:19:59 +0100

    fixat login knapp

--- src/router.js
+++ src/router.js
@@ -14,15 +14,6 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import( /* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
       path: "/registermember",
       name: "register",
       // route level code-splitting
@@ -32,7 +23,6 @@ export default new Router({
         import( /* webpackChunkName: "register" */ "./views/RegisterMember.vue")
     },
     {
-      name: "mina sidor",
       path: "/mypages",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
